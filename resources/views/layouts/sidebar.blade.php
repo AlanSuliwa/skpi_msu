4e55d428f09c50d9f2c399c2583b7719c01a22d0
unknown <alansuliwa8gmail.com>
2023-06-24 22:14:33 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="dist/img/logo-polsri.png" alt="PolsriLogo" class="brand-image img-circle elevation-3"
             style="opacity: 0.8" />
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">SKPI</span>
     </a>
 
     <!-- Sidebar -->
@@ -87,7 +87,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="" class="nav-link">
+                    <a href="{{ route('user.index') }} class="nav-link">
                         <i class="nav-icon 	fas fa-user"></i>
                         <p>
                             Data Pengguna
